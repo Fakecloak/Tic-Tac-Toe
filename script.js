d1801f500fdfbfae4440cad5f7ca2d2a9da42f85
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-01-09 11:40:26 +0530

    add logic and basic css

--- script.js
+++ script.js
@@ -52,36 +52,89 @@ Gameboard.prototype.checkWinner = function () {
 }
 
 Gameboard.prototype.handleTurn = function (position) {
-    if (!this.playTurn(position)) return;
+
+    if (this.gameOver) return { status: "game over" };
+
+    if (!this.playTurn(position)) return { status: "invalid" };
 
     this.printBoard();
 
     if (this.checkWinner()) {
-        console.log(`player ${this.currentPlayer} wins!`)
-        return;
+        console.log(`player ${this.currentPlayer} wins!`);
+        this.gameOver = true;
+        return { status: "win" };
     }
 
     if (this.checkTie()) {
         console.log("It's a tie!")
-        return;
+        this.gameOver = true;
+        return { status: "tie" };
     }
 
     this.switchPlayer();
+
+    return {
+        status: "continue"
+    };
 }
 
 // main function
 function Gameboard() {
     this.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
     this.currentPlayer = 'X';
-
-
-
-    this.handleTurn();
-
+    this.gameOver = false;
 }
 
 
 
 // func call 
-let x = new Gameboard();
-x.handleTurn(1);
+let game = new Gameboard();
+
+const buttons = document.querySelectorAll("button");
+const statusTxt = document.getElementById("status");
+const resetBtn = document.getElementById("reset");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const position = button.getAttribute("data-index");
+        const result = game.handleTurn(position);
+
+        button.textContent = game.board[position];
+
+        if (result.status === "invalid") {
+            statusTxt.textContent = "Cell already taken!";
+            return;
+        }
+
+        if (result.status === "win") {
+            statusTxt.textContent = `player ${game.currentPlayer} wins!`;
+            return;
+        }
+
+        if (result.status === "tie") {
+            statusTxt.textContent = "It's a tie!";
+            return;
+        }
+
+        if (result.status === "continue") {
+            statusTxt.textContent = `player ${game.currentPlayer}'s turn`;
+        }
+    })
+});
+
+
+resetBtn.addEventListener("click", () => {
+
+    // reset game data
+    game.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+    game.currentPlayer = "X";
+    game.gameOver = false;
+
+    // clear board UI
+    buttons.forEach(btn => {
+        btn.textContent = "";
+    });
+
+    // reset status text
+    statusTxt.textContent = "Player X's turn";
+});
